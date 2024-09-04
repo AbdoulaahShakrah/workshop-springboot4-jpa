5194e46a3fec9964ea2257d3f457376a402f0f45
Abdoulaah Shakrah <20696@stu.ipbeja.pt>
2024-09-04 17:17:21 +0100

    Service layer, component registration

--- src/main/java/com/abdullah/course/repositories/UserRepository.java
+++ src/main/java/com/abdullah/course/repositories/UserRepository.java
@@ -1,8 +1,10 @@
 package com.abdullah.course.repositories;
 
 import org.springframework.data.jpa.repository.JpaRepository;
+import org.springframework.stereotype.Repository;
 
 import com.abdullah.course.entities.User;
 
+@Repository
 public interface UserRepository extends JpaRepository<User, Long>{
-}
+} 
