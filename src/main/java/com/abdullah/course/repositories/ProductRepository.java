21da90bbf6ff52dfc7f4160abc202b14dcc31cd0
Abdoulaah Shakrah <20696@stu.ipbeja.pt>
2024-09-05 22:03:40 +0100

    Product-OrderItem one-to-many association

--- src/main/java/com/abdullah/course/repositories/ProductRepository.java
+++ src/main/java/com/abdullah/course/repositories/ProductRepository.java
@@ -3,8 +3,6 @@ package com.abdullah.course.repositories;
 import org.springframework.data.jpa.repository.JpaRepository;
 import org.springframework.stereotype.Repository;
 
-import com.abdullah.course.entities.Category;
-import com.abdullah.course.entities.Order;
 import com.abdullah.course.entities.Product;
 
 @Repository
