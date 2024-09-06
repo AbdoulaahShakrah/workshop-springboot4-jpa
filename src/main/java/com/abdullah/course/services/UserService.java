ed0f23e2e2bfca3b066363a1cbebc56afe1a7b5e
Abdoulaah Shakrah <20696@stu.ipbeja.pt>
2024-09-07 00:49:12 +0100

    Exception handling - findById

--- src/main/java/com/abdullah/course/services/UserService.java
+++ src/main/java/com/abdullah/course/services/UserService.java
@@ -8,6 +8,7 @@ import org.springframework.stereotype.Service;
 
 import com.abdullah.course.entities.User;
 import com.abdullah.course.repositories.UserRepository;
+import com.abdullah.course.services.exceptions.ResourceNotFoundException;
 
 @Service
 public class UserService {
@@ -20,7 +21,7 @@ public class UserService {
 	}
 	public User findById(Long id) {
 		Optional<User> obj = userRepository.findById(id);
-		return obj.get();
+		return obj.orElseThrow(() -> new ResourceNotFoundException(id));
 	}
 	public User insert(User obj) {
 		return userRepository.save(obj);
