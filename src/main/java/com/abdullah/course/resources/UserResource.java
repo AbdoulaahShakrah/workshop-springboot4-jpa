21ff2f0339ca90033c1c83803c2b26b03800eb5d
Abdoulaah Shakrah <20696@stu.ipbeja.pt>
2024-09-06 15:39:18 +0100

    User delete

--- src/main/java/com/abdullah/course/resources/UserResource.java
+++ src/main/java/com/abdullah/course/resources/UserResource.java
@@ -5,6 +5,7 @@ import java.util.List;
 
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.http.ResponseEntity;
+import org.springframework.web.bind.annotation.DeleteMapping;
 import org.springframework.web.bind.annotation.GetMapping;
 import org.springframework.web.bind.annotation.PathVariable;
 import org.springframework.web.bind.annotation.PostMapping;
@@ -14,6 +15,7 @@ import org.springframework.web.bind.annotation.RestController;
 import org.springframework.web.servlet.support.ServletUriComponentsBuilder;
 
 import com.abdullah.course.entities.User;
+import com.abdullah.course.repositories.UserRepository;
 import com.abdullah.course.services.UserService;
 
 import jakarta.websocket.server.PathParam;
@@ -44,5 +46,11 @@ public class UserResource {
 		URI uri = ServletUriComponentsBuilder.fromCurrentRequest().path("/{id}")
 				.buildAndExpand(obj.getId()).toUri();
 		return ResponseEntity.created(uri).body(obj);
+	}
+	
+	@DeleteMapping(value= "/{id}")
+	public ResponseEntity<Void> delete(@PathVariable Long id){
+		userService.delete(id);
+		return ResponseEntity.noContent().build();
 	}
 }
